dfca44527f1c1e30560579b6772ba030968c16dc
kalidawel <126219309+kalidawel@users.noreply.github.com>
2023-03-12 08:29:04 -0400

    doing eclipse

---
+++ javasimpleproject101/src/javasimpleproject101/example101.java
@@ -0,0 +1,16 @@
+package javasimpleproject101;
+
+public class example101 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("print onething");
+	System.out.println("print onething");
+	System.out.println("print onething");
+	System.out.println("print onething");
+	System.out.println("print onething");
+	System.out.println("print onething");
+
+	}
+
+}
---
+++ javasimpleproject101/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author kabgo
+ *
+ */
+module javasimpleproject101 {
+}
